d6560579b9d2de184f8bc6e5eaf71b28d05f9f44
Jason Couture <jasonc@alertr.info>
2022-10-25 19:05:29 -0400

    Create json deserializer service

---
+++ EasyCards/Services/JsonDeserializer.cs
@@ -0,0 +1,29 @@
+using System.Text.Json;
+using Native = System.Text.Json.JsonSerializer;
+
+namespace EasyCards.Services
+{
+    public class JsonDeserializer : IJsonDeserializer
+    {
+        public JsonDeserializer()
+        {
+            Options = new()
+            {
+                AllowTrailingCommas = true,
+                IncludeFields = true,
+                PropertyNameCaseInsensitive = true,
+                ReadCommentHandling = JsonCommentHandling.Skip,
+                WriteIndented = true
+            };
+            Options.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter());
+        }
+        
+
+        public T Deserialize<T>(string json)
+        {
+            return Native.Deserialize<T>(json, Options);
+        }
+
+        public JsonSerializerOptions Options;
+    }
+}
