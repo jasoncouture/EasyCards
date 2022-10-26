0da027e6172dab95f20b92be6df57aad843bb8a5
Jason Couture <jasonc@alertr.info>
2022-10-25 21:26:22 -0400

    Create ValidationModule

---
+++ EasyCards/Validation/ValidationModule.cs
@@ -0,0 +1,8 @@
+using EasyCards.Validation.Validators;
+using StrongInject;
+
+namespace EasyCards.Validation;
+
+[Register(typeof(ValidationAggregator), Scope.SingleInstance, typeof(IValidationAggregator))]
+[RegisterModule(typeof(ValidatorsModule))]
+public class ValidationModule { }
